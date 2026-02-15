8907ce2c4de99ea62d335beecc3c4a051a7c33f9
Nicos Pohland <n.p.pohland@gmail.com>
2026-02-15 16:22:00 +0100

    Make theme switch smaller and more elegant

--- apps/web/app/components/ThemeSwitch.tsx
+++ apps/web/app/components/ThemeSwitch.tsx
@@ -16,8 +16,8 @@ export function ThemeSwitch({
         border: '1px solid var(--border)',
         borderRadius: 999,
         padding: 2,
-        width: 70,
-        height: 32
+        width: 56,
+        height: 26
       }}
     >
       <button
@@ -33,12 +33,12 @@ export function ThemeSwitch({
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          width: 32,
-          height: 28,
+          width: 26,
+          height: 22,
           transition: 'all 0.15s ease'
         }}
       >
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="12" cy="12" r="4" fill="currentColor" />
           <path
             d="M12 5V3M12 21v-2M5 12H3M21 12h-2M7 7L5.5 5.5M19 19l-1.5-1.5M7 17l-1.5 1.5M19 5l-1.5 1.5"
@@ -62,12 +62,12 @@ export function ThemeSwitch({
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          width: 32,
-          height: 28,
+          width: 26,
+          height: 22,
           transition: 'all 0.15s ease'
         }}
       >
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M12 3a7.5 7.5 0 0 0 9 9 9 9 0 1 1-9-9z"
             fill="currentColor"
